bf964c21ee46b5ee61e703365569a107accc7e20
Lucas <flucasyoel@gmail.com>
2024-10-14 11:56:38 -0300

    feat(api): :sparkles: Creation enum for endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,7 +2,6 @@ export enum ENDPOINTS {
   ORDERS = 'orders',
   ORDERS_PDF = 'orders/pdf/',
   ORDERS_RANGE = 'orders/range',
-
   USERS = 'users',
   SHEET_CATEGORIES = 'sheets/products/categories',
   SHEET_PRODUCTS = 'sheets/products',
