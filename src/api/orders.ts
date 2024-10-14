4791768cbe896f37fac5cd651df392d2171fed81
Lucas <flucasyoel@gmail.com>
2024-10-14 12:21:37 -0300

    feat(api): :sparkles: Creation methods for endpoint orders

---
+++ src/api/orders.ts
@@ -0,0 +1,22 @@
+import { BASE_URL, instance } from './base.api';
+import { ENDPOINTS } from './endpoints';
+import { Order } from './interfaces/order.interface';
+
+const getOrders = (): Promise<Order> => instance.get(ENDPOINTS.ORDERS);
+
+const getPDFOrders = (id: string) => (window.location.href = `${BASE_URL}/${ENDPOINTS.ORDERS_PDF}${id}`);
+
+const deleteOrder = (id: string) => instance.post(`${ENDPOINTS.ORDERS}/${id}`);
+
+const updateOrder = (id: string, order: any) => instance.put(`${ENDPOINTS.ORDERS}/${id}`, order);
+
+const getOrdersForRange = (startDate: string, endDate: string): Promise<any> =>
+  instance.get(`${ENDPOINTS.ORDERS_RANGE}?startDate=${startDate}&endDate=${endDate}`);
+
+export const orders = {
+  updateOrder,
+  getOrders,
+  getPDFOrders,
+  deleteOrder,
+  getOrdersForRange,
+};
