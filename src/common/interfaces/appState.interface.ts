330b3f1a73dc77ae7c1a50536bede589801e9369
Lucas <flucasyoel@gmail.com>
2024-10-14 11:59:27 -0300

    feat(common): :sparkles: Add type order in state

--- src/common/interfaces/appState.interface.ts
+++ src/common/interfaces/appState.interface.ts
@@ -1,10 +1,12 @@
 import { StatisticsResponse, UserResponse } from './api/response.interface';
 import { User } from './user/user.interface';
 import { LocalStorageUser } from './user/user.interface';
+import { Order } from '../../api/interfaces/order.interface';
 
 export default interface AppState {
   user: User | null;
   statisticsResponse: StatisticsResponse | null;
   localStorageUser: LocalStorageUser | '';
   dateRange: Range[];
+  order: Order[] | null;
 }
