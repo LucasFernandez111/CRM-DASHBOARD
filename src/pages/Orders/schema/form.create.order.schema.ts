1115c371f73bdb0f1f9948f728dca5e41416fa12
Lucas <flucasyoel@gmail.com>
2024-10-20 01:25:10 -0300

    feat(pages): :sparkles: Creation schema for zod

---
+++ src/pages/Orders/schema/form.create.order.schema.ts
@@ -0,0 +1,68 @@
+import { PaymentMethod, OrderStatus, PaymentStatus } from '@/api';
+import { z } from 'zod';
+
+const itemSchema = z.object({
+  category: z.string().default(''),
+  subcategory: z.string().default(''),
+  price: z
+    .string()
+    .refine((val) => !isNaN(parseFloat(val)), { message: 'La cantidad debe ser un número' })
+    .transform((val) => parseFloat(val)),
+  quantity: z
+    .string()
+    .refine((val) => !isNaN(parseFloat(val)), { message: 'La cantidad debe ser un número' })
+    .transform((val) => parseFloat(val)),
+  description: z.string().default(''),
+});
+
+const addressSchema = z.object({
+  street: z.string().default(''),
+  city: z.string().default(''),
+  postalCode: z.string().default(''),
+  country: z.string().default(''),
+});
+
+const customerSchema = z.object({
+  name: z.string().default(''),
+  phone: z.string().default(''),
+  address: addressSchema,
+});
+
+const paymentDetailsSchema = z.object({
+  method: z.nativeEnum(PaymentMethod),
+});
+
+export const createOrderSchema = z.object({
+  customer: customerSchema,
+  items: z.array(itemSchema),
+  paymentDetails: paymentDetailsSchema,
+  orderStatus: z.nativeEnum(OrderStatus).default(OrderStatus.PENDIENTE),
+});
+
+export const defaultCreateOrderValues: CreateOrderType = {
+  customer: {
+    name: '',
+    phone: '',
+    address: {
+      street: '',
+      city: '',
+      postalCode: '',
+      country: '',
+    },
+  },
+  items: [
+    {
+      category: '',
+      subcategory: '',
+      description: '',
+      quantity: 1,
+      price: 1,
+    },
+  ],
+  paymentDetails: {
+    method: PaymentMethod.TRANSFERENCIA,
+  },
+  orderStatus: OrderStatus.PENDIENTE,
+};
+
+export type CreateOrderType = z.infer<typeof createOrderSchema>;
