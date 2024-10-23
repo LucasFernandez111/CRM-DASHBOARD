8d00cbd77c9e708d665a671c5f9c5df4ee1c899b
Lucas <flucasyoel@gmail.com>
2024-10-23 11:22:55 -0300

    refactor: :fire: Remove file

--- src/pages/ventas/Ventas.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export type VentasPageProps = {};
-
-const VentasPage: React.FC<VentasPageProps> = ({}) => {
-  return <div>Ventas works!</div>;
-};
-
-export default VentasPage;
