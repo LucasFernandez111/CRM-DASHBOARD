aa60a66500fa9877a27b838e50c672ce3a132552
Lucas <flucasyoel@gmail.com>
2024-10-25 18:32:13 -0300

    refactor(component): :lipstick: Changes in text date of sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,20 +1,16 @@
+import { DateContext } from '@/context/DateContextProvider';
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DateRangeComp from './DateRangeComp';
-import { DateContext } from '@/context/DateContextProvider';
 export const SideBar: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const navigate = useNavigate();
   const { dateRange } = useContext(DateContext);
+  const navigate = useNavigate();
 
-  const handleItemClick = (index: number | null) => {
-    setActiveIndex(index);
-  };
+  const handleItemClick = (index: number | null) => setActiveIndex(index);
 
-  const handleNavigation = (section: string) => {
-    navigate(section);
-  };
+  const handleNavigation = (section: string) => navigate(section);
 
   return (
     <aside className="xl:col-span-1 lg:bg-black sm:bg-slate-500 xl:bg-white">
@@ -28,7 +24,7 @@ export const SideBar: React.FC = () => {
           </li>
           <li
             className="cursor-pointer hover:text-sky-400 transition duration-200"
-            onClick={() => handleNavigation('orders')}
+            onClick={() => handleNavigation('pedidos')}
           >
             PEDIDOS
           </li>
@@ -40,15 +36,17 @@ export const SideBar: React.FC = () => {
           </li>
         </ul>
         <div
-          className="cursor-pointer"
+          className="cursor-pointer  text-customSteelblue font-extrabold flex items-center justify-center flex-col "
           onClick={() => {
             handleItemClick(null);
             setIsOpen(!isOpen);
           }}
         >
-          <h2 className="text-xl text-customSteelblue font-extrabold mb-2">FECHA</h2>
+          <h2 className="text-2xl mb-2">FECHA</h2>
           <DateRangeComp isOpen={isOpen} setIsOpen={setIsOpen} />
-          <h4 className="text-sm mt-2">{`${dateRange?.startDate || ''} - ${dateRange?.endDate || ''}`}</h4>
+          <h4 className="text-lg mt-2">{dateRange?.startDate}</h4>
+          <span>HASTA</span>
+          <h4 className="text-lg mt-2">{dateRange?.endDate}</h4>
         </div>
       </div>
     </aside>
