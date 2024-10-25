46651d72112f73331a33ee8ef55e00ef9c0913a1
Lucas <flucasyoel@gmail.com>
2024-10-25 18:40:52 -0300

    feat(pages): :sparkles: Creation component for show menu

---
+++ src/pages/menu/components/data-table/DataTable/DataTable.tsx
@@ -0,0 +1,110 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useState } from 'react';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const DataTable = <TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    },
+  });
+  return (
+    <div className="rounded-md border ">
+      {/* FILTRADO DE DATOS */}
+      <div className="flex items-center p-4 gap-2 justify-around">
+        <Input
+          placeholder="Filtrado por categoria"
+          value={(table.getColumn('category')?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn('category')?.setFilterValue(event.target.value)}
+          className="max-w-[200px] border-customSteelblue"
+        />
+
+        <Input
+          placeholder="Filtrado por subcategoria"
+          value={(table.getColumn('subcategory')?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn('subcategory')?.setFilterValue(event.target.value)}
+          className="max-w-[200px] border-customSteelblue"
+        />
+      </div>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+
+      <div className="flex items-center justify-around space-x-2 py-4">
+        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+          Previous
+        </Button>
+        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+          Next
+        </Button>
+      </div>
+      <div className="flex-1 text-md font-semibold text-muted-foreground p-2 ">
+        {table.getFilteredSelectedRowModel().rows.length} de {table.getFilteredRowModel().rows.length} filas
+        seleccionadas
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
