16f041bad99b0f38363dab1ba93ddb6e607f4647
Lucas <flucasyoel@gmail.com>
2024-10-25 18:37:51 -0300

    feat(pages): :sparkles: Create component card with statistics

---
+++ src/pages/sales/components/CardWithStatistics/CardWithStatistics.tsx
@@ -0,0 +1,22 @@
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+export type CardWithStatisticsProps = {
+  amount: string | number;
+  description: string;
+  className?: string;
+};
+
+const CardWithStatistics: React.FC<CardWithStatisticsProps> = ({ amount, description, className }) => {
+  return (
+    <Card className={cn('w-72 h-40  text-center flex items-center justify-center rounded-4xl p-2', className)}>
+      <CardHeader>
+        <CardTitle className="text-3xl font-extrabold text-customSteelblue">{amount} </CardTitle>
+        <CardDescription className="text-xl font-extrabold text-customSteelblue">{description}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default CardWithStatistics;
