5292295ef5cd4c88d26484082dcb1e102c6fc26a
Lucas <flucasyoel@gmail.com>
2024-10-25 18:39:37 -0300

    feat(pages): :sparkles: Creation menu page

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,3 +1,34 @@
+import { Card } from '@/components/ui/card';
+import { columns, DataTable } from './components/data-table/DataTable';
+import { useNotification, useProducts } from '@/hooks';
+import { useEffect, useState } from 'react';
+import { SheetData } from './components/data-table/DataTable/columns';
+import { structureProductsData } from './utils';
+
 export const MenuPage = () => {
-  return <div>Menu works!</div>;
+  const { products, error } = useProducts();
+  const { alertError, alertInfo } = useNotification();
+  const [productsData, setProductsData] = useState<SheetData[]>([
+    {
+      category: '-',
+      subcategory: '-',
+      price: '0',
+      stock: 0,
+    },
+  ]);
+
+  useEffect(() => {
+    if (!products && error) alertError('Error al cargar los productos');
+    if (products.length < 0) alertInfo('No hay productos');
+
+    setProductsData(structureProductsData(products));
+  }, [products, error]);
+
+  return (
+    <main className=" col-span-11   h-full max-h-screen overflow-auto bg-customSteelblue p-4">
+      <Card>
+        <DataTable columns={columns} data={productsData} />
+      </Card>
+    </main>
+  );
 };
