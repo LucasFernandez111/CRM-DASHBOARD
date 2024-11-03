015a9c8dc7f51eea7922e754b119c120f545e9a1
Lucas <flucasyoel@gmail.com>
2024-11-03 18:16:15 -0300

    feat ✨: add `UserUpdateSchema` for user profile update validation

---
+++ src/pages/Orders/schema/form.update.user.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+// Esquema de validación para el formulario de actualización de usuario
+
+export const UserUpdateSchema = z.object({
+  phone: z.string().optional(),
+  address: z.string().optional(),
+  company: z.string().optional(),
+  sheetId: z.string().optional(),
+});
+
+//Lo convertimos a un type
+export type UserUpdateType = z.infer<typeof UserUpdateSchema>;
