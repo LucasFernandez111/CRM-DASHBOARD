1dcfa57a14ac8b460f4b2dcfa6299d1a04389e83
Lucas <flucasyoel@gmail.com>
2024-11-03 18:14:51 -0300

    feat ✨: create `FormCheckBoxStatusOrder` component for updating order status

---
+++ src/pages/panel/components/FormCheckBoxStatusOrder/FormCheckBoxStatusOrder.tsx
@@ -0,0 +1,61 @@
+'use client';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form';
+import { FormStatusOrderSchema, FormStatusOrderType } from '@/pages/orders/schema/form-status-order.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Checkbox } from '@/components/ui/checkbox';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { orders, OrderStatus } from '@/api';
+import { useNotification } from '@/hooks';
+
+export type FormCheckBoxStatusOrderProps = {
+  _id: string;
+  orderStatus: OrderStatus;
+};
+
+const FormCheckBoxStatusOrder: React.FC<FormCheckBoxStatusOrderProps> = ({ orderStatus, _id }) => {
+  const { alertSuccess, alertError } = useNotification();
+  const form = useForm<FormStatusOrderType>({
+    resolver: zodResolver(FormStatusOrderSchema),
+    defaultValues: {
+      orderStatus: orderStatus,
+    },
+  });
+
+  const onSubmit = async (data: FormStatusOrderType) => {
+    try {
+      await orders.updateOrder(_id, data);
+
+      alertSuccess('Orden actualizada exitosamente');
+      window.location.reload();
+    } catch (error) {
+      alertError('Error al actualizar la orden');
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-row justify-center items-center space-x-4">
+        <FormField
+          control={form.control}
+          name="orderStatus"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-center justify-center space-x-4 ">
+              <FormControl className="">
+                <Checkbox
+                  className="size-10"
+                  type="submit"
+                  checked={field.value === OrderStatus.ENTREGADO}
+                  onCheckedChange={(checked) => field.onChange(checked ? OrderStatus.ENTREGADO : OrderStatus.PENDIENTE)}
+                />
+              </FormControl>
+              <FormLabel className="text-2xl font-bold">{orderStatus}</FormLabel>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default FormCheckBoxStatusOrder;
