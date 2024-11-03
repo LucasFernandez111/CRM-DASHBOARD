e37b1c39fa8900318b2d21f98285f745457f77ed
Lucas <flucasyoel@gmail.com>
2024-11-03 18:16:56 -0300

    refactor ♻️: rename `amount` prop to `title` in `CardWithStatistics` for clarity

--- src/pages/sales/components/CardWithStatistics/CardWithStatistics.tsx
+++ src/pages/sales/components/CardWithStatistics/CardWithStatistics.tsx
@@ -3,16 +3,16 @@ import { cn } from '@/lib/utils';
 import React from 'react';
 
 export type CardWithStatisticsProps = {
-  amount: string | number;
+  title: string | number;
   description: string;
   className?: string;
 };
 
-const CardWithStatistics: React.FC<CardWithStatisticsProps> = ({ amount, description, className }) => {
+const CardWithStatistics: React.FC<CardWithStatisticsProps> = ({ title, description, className }) => {
   return (
     <Card className={cn('w-72 h-40  text-center flex items-center justify-center rounded-4xl p-2', className)}>
       <CardHeader>
-        <CardTitle className="text-3xl font-extrabold text-customSteelblue">{amount} </CardTitle>
+        <CardTitle className="text-3xl font-extrabold text-customSteelblue">{title} </CardTitle>
         <CardDescription className="text-xl font-extrabold text-customSteelblue">{description}</CardDescription>
       </CardHeader>
     </Card>
