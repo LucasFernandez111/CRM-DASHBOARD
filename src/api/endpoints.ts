33908f078a76c83edf1e8faaa6c4d3abe4f0762a
Lucas <flucasyoel@gmail.com>
2024-11-03 16:20:46 -0300

    feat(api): :sparkles: Add logout endpoint in enum

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -2,7 +2,10 @@ export enum ENDPOINTS {
   ORDERS = 'orders',
   ORDERS_PDF = 'orders/pdf/',
   ORDERS_RANGE = 'orders/range',
+  STATISTICS_SALES = 'orders/statistics/sales',
+  STATISTICS_TOP_ORDERS = 'orders/statistics/top-order',
   USERS = 'users',
   SHEET_CATEGORIES = 'sheets/products/categories',
   SHEET_PRODUCTS = 'sheets/products',
+  LOGOUT = 'auth/logout',
 }
