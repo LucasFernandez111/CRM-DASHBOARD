1f2f04f116b224dc6f57017a9920d4b92315df27
Lucas <flucasyoel@gmail.com>
2024-11-03 16:27:29 -0300

    feat ✨: add getUser function to retrieve user information

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,8 +1,17 @@
+import { UserUpdateType } from '@/pages/orders/schema/form.update.user.schema';
 import { instance } from './base.api';
 import { ENDPOINTS } from './endpoints';
+import { AxiosResponse } from 'axios';
+import { User } from './interfaces';
 
-const getUser = () => instance.get(ENDPOINTS.USERS);
+interface ReponseBody {
+  user: User;
+}
+const getUser = (): Promise<AxiosResponse<ReponseBody>> => instance.get(ENDPOINTS.USERS);
+
+const updateUser = (user: UserUpdateType) => instance.put(ENDPOINTS.USERS, user);
 
 export const users = {
   getUser,
+  updateUser,
 };
