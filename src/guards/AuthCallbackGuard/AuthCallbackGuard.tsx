fdb958af8ac61a37bfcb9f0feab3eb63543a86c8
Lucas <flucasyoel@gmail.com>
2024-11-03 16:33:28 -0300

    feat ✨: add `AuthCallbackGuard` component for user authentication redirection

---
+++ src/guards/AuthCallbackGuard/AuthCallbackGuard.tsx
@@ -0,0 +1,24 @@
+import { users } from '@/api';
+import { createUser } from '@/redux/states';
+import { PrivateRoutes, PublicRoutes } from '@/routes/routes';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export const AuthCallbackGuard = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    users
+      .getUser()
+      .then((r) => {
+        dispatch(createUser(r.data?.user));
+        navigate(PrivateRoutes.SALES);
+      })
+      .catch(() => navigate(PublicRoutes.LOGIN));
+  }, []);
+
+  return <></>;
+};
