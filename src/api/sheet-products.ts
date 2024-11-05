2cc8e23e2da7ebcd1964a7bc2e18d026773a83a3
Lucas <flucasyoel@gmail.com>
2024-11-05 01:03:51 -0300

    feat ✨: add functions to fetch sheet products and categories by `sheetId`

--- src/api/sheet-products.ts
+++ src/api/sheet-products.ts
@@ -1,8 +1,8 @@
 import { instance } from './base.api';
 import { ENDPOINTS } from './endpoints';
 
-const getSheetProducts = () => instance.get(ENDPOINTS.SHEET_PRODUCTS);
-const getSheetCategories = () => instance.get(ENDPOINTS.SHEET_CATEGORIES);
+const getSheetProducts = (sheetId: string) => instance.get(`${ENDPOINTS.SHEET_PRODUCTS}/${sheetId}`);
+const getSheetCategories = (sheetId: string) => instance.get(`${ENDPOINTS.SHEET_CATEGORIES}/${sheetId}`);
 
 export const sheetProducts = {
   getSheetCategories,
