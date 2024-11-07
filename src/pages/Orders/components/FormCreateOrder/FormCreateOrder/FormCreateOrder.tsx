1d0eb99c04e4b384099082722400397013640cdf
Lucas <flucasyoel@gmail.com>
2024-11-07 15:45:59 -0300

    ✨ feat: added `onRefresh` prop to `FormCreateOrder` component for refresh functionality.

--- src/pages/Orders/components/FormCreateOrder/FormCreateOrder/FormCreateOrder.tsx
+++ src/pages/Orders/components/FormCreateOrder/FormCreateOrder/FormCreateOrder.tsx
@@ -14,7 +14,11 @@ import { useFieldArray, useForm } from 'react-hook-form';
 import { FormInputField } from '../../../../../components/FormInputField';
 import { FormSelectField } from '../../../../../components/FormSelectField';
 
-const FormCreateOrder: React.FC<{}> = ({}) => {
+type FormCreateOrderProps = {
+  onRefresh: () => void;
+};
+
+const FormCreateOrder: React.FC<FormCreateOrderProps> = ({ onRefresh }) => {
   const form = useForm<CreateOrderType>({
     resolver: zodResolver(createOrderSchema),
     defaultValues: defaultCreateOrderValues,
@@ -30,9 +34,12 @@ const FormCreateOrder: React.FC<{}> = ({}) => {
   const onSubmit = async (values: CreateOrderType) => {
     try {
       await orders.createOrder(values);
+
       alertSuccess('Orden creada correctamente');
+
       form.reset(defaultCreateOrderValues);
-      window.location.reload();
+
+      onRefresh();
     } catch (error) {
       alertError('Error al crear la orden');
     }
@@ -40,56 +47,45 @@ const FormCreateOrder: React.FC<{}> = ({}) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-4 gap-5">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-3   gap-5 ">
         {/* <-- Customer --> */}
         <FormField
           control={form.control}
           name="customer.name"
-          render={({ field }) => <FormInputField label="Nombre" field={field} type="text" placeholder="Nombre" />}
-        />
-
-        <FormField
-          control={form.control}
-          name="customer.phone"
-          render={({ field }) => <FormInputField label="Teléfono" field={field} type="text" placeholder="Teléfono" />}
-        />
-
-        {/* <-- Address --> */}
-        <FormField
-          control={form.control}
-          name="customer.address.street"
-          render={({ field }) => <FormInputField label="Calle" field={field} type="text" placeholder="Calle 8998" />}
-        />
-
-        <FormField
-          control={form.control}
-          name="customer.address.city"
-          render={({ field }) => <FormInputField label="Ciudad" field={field} type="text" placeholder="Ciudad" />}
-        />
-
-        <FormField
-          control={form.control}
-          name="customer.address.postalCode"
           render={({ field }) => (
-            <FormInputField label="Código postal" field={field} type="text" placeholder="Código postal" />
+            <FormInputField label="Nombres" field={field} type="text" placeholder="Nombres" required />
           )}
         />
 
         <FormField
           control={form.control}
-          name="customer.address.country"
-          render={({ field }) => <FormInputField label="País" field={field} type="text" placeholder="País" />}
+          name="customer.phone"
+          render={({ field }) => (
+            <FormInputField label="Telefono" type="text" field={field} placeholder="Telefono" required />
+          )}
         />
+
+        <FormField
+          control={form.control}
+          name="customer.address.street"
+          render={({ field }) => (
+            <FormInputField label="Direccion" type="text" field={field} placeholder="Calle 123" required />
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="customer.address.city"
+          render={({ field }) => (
+            <FormInputField label="Localidad" type="text" field={field} placeholder="CABA" required />
+          )}
+        />
+
         <FormField
           control={form.control}
           name="paymentDetails.method"
           render={({ field }) => (
-            <FormSelectField
-              placeholder="Método de pago"
-              label="Método de pago"
-              field={field}
-              options={Object.values(PaymentMethod)}
-            />
+            <FormSelectField label="Metodo de pago" field={field} options={Object.values(PaymentMethod)} />
           )}
         />
 
@@ -97,99 +93,93 @@ const FormCreateOrder: React.FC<{}> = ({}) => {
           control={form.control}
           name="orderStatus"
           render={({ field }) => (
-            <FormSelectField
-              placeholder="Estado"
-              label="Estado del pedido"
-              field={field}
-              options={Object.values(OrderStatus)}
-            />
+            <FormSelectField label="Estado de la orden" field={field} options={Object.values(OrderStatus)} />
           )}
         />
 
-        {/* <-- Items --> */}
-        {fields.map((field, index) => (
-          <section key={field.id} className="col-span-4 grid grid-cols-2 gap-2 border rounded-lg border-gray-300 p-2">
-            <FormField
-              control={form.control}
-              name={`items.${index}.category`}
-              render={({ field }) => (
-                <FormSelectField placeholder="CATEGORIA" label="CATEGORIA" field={field} options={categories} />
-              )}
-            />
-            <FormField
-              control={form.control}
-              name={`items.${index}.subcategory`}
-              render={({ field }) => (
-                <FormSelectField
-                  placeholder="SUBCATEGORIA"
-                  label="SUBCATEGORIA"
-                  field={field}
-                  options={subcategories}
-                />
-              )}
-            />
+        <section className=" col-span-3 h-56 overflow-y-auto space-y-8">
+          {fields.map((field, i) => (
+            <div key={i} className="border rounded-2xl p-6 bg-white shadow-xl grid grid-cols-4  space-x-3 ">
+              <FormField
+                control={form.control}
+                name={`items.${i}.category`}
+                render={({ field }) => (
+                  <FormSelectField placeholder="CATEGORIA" label="CATEGORIA" field={field} options={categories} />
+                )}
+              />
+              <FormField
+                control={form.control}
+                name={`items.${i}.subcategory`}
+                render={({ field }) => (
+                  <FormSelectField
+                    placeholder="SUBCATEGORIA"
+                    label="SUBCATEGORIA"
+                    field={field}
+                    options={subcategories}
+                  />
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name={`items.${index}.quantity`}
-              render={({ field }) => (
-                <FormSelectField
-                  placeholder="0"
-                  label="CANTIDAD"
-                  field={field}
-                  options={Array.from({ length: 10 }, (_, i) => (i + 1).toString())} //Crea un Array con valores del 1 a 10
-                />
-              )}
-            />
+              <FormField
+                control={form.control}
+                name={`items.${i}.quantity`}
+                render={({ field }) => (
+                  <FormSelectField
+                    placeholder="0"
+                    label="CANTIDAD"
+                    field={field}
+                    options={Array.from({ length: 10 }, (_, i) => (i + 1).toString())} //Crea un Array con valores del 1 a 10
+                  />
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name={`items.${index}.price`}
-              render={({ field }) => <FormInputField placeholder="0" label="Precio" field={field} type="number" />}
-            />
+              <FormField
+                control={form.control}
+                name={`items.${i}.price`}
+                render={({ field }) => <FormInputField placeholder="0" label="Precio" field={field} type="number" />}
+              />
 
-            <FormField
-              control={form.control}
-              name={`items.${index}.description`}
-              render={({ field }) => (
-                <FormItem className="col-span-4">
-                  <FormLabel>Descripción</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="Descripción..." {...field}></Textarea>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </section>
-        ))}
+              <FormField
+                control={form.control}
+                name={`items.${i}.description`}
+                render={({ field }) => (
+                  <FormItem className="col-span-4">
+                    <FormLabel>Descripción</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Descripción..." {...field}></Textarea>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          ))}
+        </section>
 
         <FormField
           control={form.control}
-          name={`notes`}
+          name="notes"
           render={({ field }) => (
-            <FormItem className="col-span-4">
-              <FormLabel>NOTAS</FormLabel>
+            <FormItem className="col-span-3">
+              <FormLabel>Notas adicionales</FormLabel>
               <FormControl>
-                <Textarea placeholder="Agrega una nota" {...field}></Textarea>
+                <Textarea placeholder="Notas..." {...field}></Textarea>
               </FormControl>
             </FormItem>
           )}
         />
+
         <Button
-          className="col-span-2"
           type="button"
           onClick={() => append({ category: '', subcategory: '', quantity: 1, price: 1, description: '' })}
         >
           +
         </Button>
 
-        <Button className="col-span-2" type="button" onClick={() => fields.length > 1 && remove(fields.length - 1)}>
+        <Button type="button" onClick={() => fields.length > 1 && remove(fields.length - 1)}>
           -
         </Button>
 
-        <Button className="col-span-4" type="submit">
-          Crear Pedido
-        </Button>
+        <Button type="submit">Crear Pedido</Button>
       </form>
     </Form>
   );
