48afcc351fe189aa404f4cd6e66907b87932b60c
Lucas <flucasyoel@gmail.com>
2024-11-07 15:43:49 -0300

    feat ✨: added `filterOrdersForStatus` utility function to filter orders based on their status.

--- src/pages/Orders/components/OrderCard/OrderCard.tsx
+++ src/pages/Orders/components/OrderCard/OrderCard.tsx
@@ -1,14 +1,13 @@
 'use client';
-import { Order, orders } from '@/api';
+import { Order, OrderItem, orders } from '@/api';
 import { DialogComp } from '@/components/DialogComp';
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { useNotification } from '@/hooks';
 import { FormCheckBoxStatusOrder } from '@/pages/panel/components/FormCheckBoxStatusOrder';
 import React from 'react';
 import { BiSolidShow } from 'react-icons/bi';
 import { IoPrintSharp } from 'react-icons/io5';
-import { LuCalendarClock } from 'react-icons/lu';
 import { MdDeleteForever } from 'react-icons/md';
 import { ShowFullOrder } from './components/ShowFullOrder';
 
@@ -45,89 +44,76 @@ const OrderCard: React.FC<OrderCardProps> = ({
     }
   };
   return (
-    <Card className="flex flex-col max-w-3xl p-4 space-y-4 ">
-      {/* Header */}
-      <CardHeader className="flex items-center justify-around flex-row">
-        {/* Círculo con Número */}
-        <div className="flex items-center justify-center bg-sky-600 size-20 text-2xl rounded-full">
-          <span className="text-white font-bold">#{orderNumber}</span>
-        </div>
-
-        {/* Fecha con Icono */}
-        <div className="flex items-center gap-2">
-          <LuCalendarClock className="text-sky-600" size={30} />
-          <time className="text-3xl font-bold">{formatDate(createdAt)}</time>
-        </div>
-
-        {/* Icono de Eliminación */}
-        <MdDeleteForever size={60} color="black" onClick={() => handleClickDelete(_id)} />
-      </CardHeader>
-
-      {/* Content */}
-      <CardContent className=" flex flex-col space-y-5  uppercase   ">
-        <div className="text-center space-y-2  tracking-tight text-3xl">
-          <h1 className="font-bold ">{customer.name}</h1>
-          <h2 className="over font-extralight">RESUMEN DEL PEDIDO</h2>
-          <Separator className="bg-slate-600" />
-        </div>
-
-        <div className="flex items-center justify-around text-xl uppercase overflow-y-auto  space-x-5">
-          {/* Detalle del Pedido */}
-          <div className="text-right space-y-2  ">
-            {items.map((item) => (
-              <>
-                <div className="flex  gap-2 ">
-                  <h3>{item.quantity}X</h3>
-
-                  <h3>{item.category}</h3>
-                </div>
-                <h3 className="text-start line-clamp-1">{item.subcategory}</h3>
-                <Separator />
-              </>
-            ))}
+    <Card>
+      <CardContent className="flex aspect-square items-center  justify-between p-6 flex-col ">
+        <header className="text-3xl font-bold flex items-center  w-full justify-between">
+          <div className="flex items-center text-white justify-center bg-sky-600 size-20   rounded-full">
+            #{orderNumber}
           </div>
-          <Separator className="bg-slate-600" orientation="vertical" />
-
-          {/* Total y Método de Pago */}
-          <div className="space-y-1">
-            <h2 className="font-bold text-4xl">${totalAmount}</h2>
-            <h3 className="font-extralight text-xl text-sa">{paymentDetails.method}</h3>
+          <div>
+            <p>{formatDate(createdAt)}</p>
           </div>
-        </div>
-
-        {/* Estado del Pedido */}
-        <div className=" text-2xl tracking-tight">
-          <FormCheckBoxStatusOrder orderStatus={orderStatus} _id={_id} />
-        </div>
-      </CardContent>
-
-      {/* Footer */}
-      <CardFooter className="flex items-center justify-center space-x-10">
-        <DialogComp
-          title="DETALLES DEL PEDIDO"
-          description="Detalles del pedido"
-          buttonTrigger={
-            <div className="cursor-pointer shadow-xl bg-sky-600 flex items-center justify-center size-20 rounded-full hover:transition duration-300 ease-in-out">
-              <BiSolidShow size={35} color="white" />
-            </div>
-          }
-        >
-          <ShowFullOrder
-            customer={customer}
-            items={items}
-            orderNumber={orderNumber}
-            totalAmount={totalAmount}
-            paymentDetails={paymentDetails}
-            onPrint={() => handleClickPDF(_id)}
+          <MdDeleteForever
+            size={65}
+            onClick={() => handleClickDelete(_id)}
+            className="cursor-pointer transition ease-in-out  hover:-translate-y-1 hover:scale-105 duration-150"
           />
-        </DialogComp>
-        <div
-          className=" cursor-pointer shadow-xl bg-sky-600 flex items-center justify-center size-20 rounded-full transition duration-300 ease-in-out"
-          onClick={() => handleClickPDF(_id)}
-        >
-          <IoPrintSharp size={35} color="white" />
-        </div>
-      </CardFooter>
+        </header>
+
+        <figure className="flex flex-col items-center text-3xl uppercase ">
+          <h1 className="font-bold">{customer.name}</h1>
+          <h2>RESUMEN DEL PEDIDO</h2>
+        </figure>
+        <Separator className="bg-slate-600  bg-opacity-35" />
+        <section className="w-full flex space-y-5  flex-col">
+          <div className="flex space-x-10 items-center justify-center ">
+            <div className="w-2/4 uppercase space-y-2 text-lg antialiased font-normal">
+              {items.map(
+                (item: OrderItem, index) =>
+                  index <= 1 && (
+                    <>
+                      <div key={index}>
+                        <p>
+                          <span className="font-bold text-xl">{item.quantity}</span> X {item.category}
+                        </p>
+                        <p className="line-clamp-1">{item.subcategory}</p>
+                      </div>
+                      <Separator className="bg-slate-600 bg-opacity-35" />
+                    </>
+                  ),
+              )}
+            </div>
+            <div className="w-2/4 space-y-1 text-center">
+              <h1 className="text-5xl font-bold">${totalAmount}</h1>
+              <h2 className="text-xl font-extralight">{paymentDetails.method}</h2>
+            </div>
+          </div>
+
+          <FormCheckBoxStatusOrder orderStatus={orderStatus} id={_id} />
+          <div className="flex  items-center justify-around">
+            <div className="flex items-center text-white justify-center bg-sky-600 size-12 transition ease-in-out  cursor-pointer hover:-translate-y-1 hover:scale-110 duration-150 rounded-full">
+              <DialogComp
+                buttonTrigger={<BiSolidShow size={30} />}
+                title="Detalle Completo del Pedido"
+                description="Revisión de todos los elementos y detalles de tu orden"
+              >
+                <ShowFullOrder
+                  customer={customer}
+                  items={items}
+                  orderNumber={orderNumber}
+                  totalAmount={totalAmount}
+                  paymentDetails={paymentDetails}
+                  onPrint={() => handleClickPDF(_id)}
+                />
+              </DialogComp>
+            </div>
+            <div className="flex items-center text-white justify-center bg-sky-600 size-12 transition ease-in-out  cursor-pointer hover:-translate-y-1 hover:scale-110 duration-150    rounded-full">
+              <IoPrintSharp size={30} onClick={() => handleClickPDF(_id)} />
+            </div>
+          </div>
+        </section>
+        <footer></footer>
+      </CardContent>
     </Card>
   );
 };
