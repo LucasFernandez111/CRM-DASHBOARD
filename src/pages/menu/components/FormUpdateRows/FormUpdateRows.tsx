136bf4e545d4490ee6f0d4a4debb5ce063fed45b
Lucas <flucasyoel@gmail.com>
2024-11-12 18:05:15 -0300

    ♻️ refactor: improve FormUpdateRows with dynamic data handling and update functionality

--- src/pages/menu/components/FormUpdateRows/FormUpdateRows.tsx
+++ src/pages/menu/components/FormUpdateRows/FormUpdateRows.tsx
@@ -1,62 +1,61 @@
+import { sheetProducts } from '@/api';
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import {
-  defaultValuesRows,
-  defaultValuesUpdateRows,
-  UpdateRowsType,
-} from '@/pages/orders/schema/form-update-order.schema';
+import { useNotification } from '@/hooks';
+import { defaultValuesUpdateRow, UpdateRowsType } from '@/pages/orders/schema/form-update-order.schema';
+import { AppStore } from '@/redux/store';
 import React from 'react';
-import { useFieldArray, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { useSelector } from 'react-redux';
 
-export type FormUpdateRowsProps = {};
+export type FormUpdateRowsProps = {
+  valueRowSelected: any;
+  rangeRowSelected: string;
+  onRefresh?: () => void;
+};
 
-const FormUpdateRows: React.FC<FormUpdateRowsProps> = () => {
+const FormUpdateRows: React.FC<FormUpdateRowsProps> = ({
+  valueRowSelected = defaultValuesUpdateRow,
+  rangeRowSelected,
+  onRefresh,
+}) => {
+  const userState = useSelector((state: AppStore) => state.user);
+  const { alertError, alertSuccess } = useNotification();
   const form = useForm<UpdateRowsType>({
-    defaultValues: defaultValuesUpdateRows,
+    defaultValues: {
+      row: valueRowSelected,
+    },
   });
 
-  const { fields, append } = useFieldArray({
-    control: form.control,
-    name: 'items',
-  });
+  const onSubmit = async ({ row }: UpdateRowsType) => {
+    try {
+      const updatedRows = [Object.values(row)];
+
+      await sheetProducts.updateSheetCategories(userState.sheetId, rangeRowSelected, updatedRows);
+
+      if (onRefresh) onRefresh();
+      alertSuccess('Celda actualizada exitosamente');
+    } catch (error) {
+      alertError('Error al actualizar la celda');
+    }
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit((data) => console.log(data))}>
-        {fields.map((field, i) => (
-          <div key={field.id} className="grid grid-cols-4 space-x-3">
-            <Input
-              placeholder="Categoria"
-              {...form.register(`items.${i}.category` as const, {
-                required: true,
-              })}
-            />
-
-            <Input
-              placeholder="Subcategoria"
-              {...form.register(`items.${i}.subcategory` as const, {
-                required: true,
-              })}
-            />
-
-            <Input
-              placeholder="Precio"
-              {...form.register(`items.${i}.price` as const, {
-                required: true,
-              })}
-            />
-
-            <Input
-              placeholder="Stock"
-              {...form.register(`items.${i}.stock` as const, {
-                required: true,
-              })}
-            />
-          </div>
-        ))}
-        <Button type="submit">Enviar</Button>
-        <Button onClick={() => append(defaultValuesRows)}>ADD</Button>
+      <form className="flex flex-col space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex space-x-4 ">
+          <Input type="text" placeholder="Categoria" {...form.register('row.category')} />
+          <Input type="text" placeholder="Subcategoria" {...form.register('row.subcategory')} />
+          <Input type="text" placeholder="Precio" {...form.register('row.price')} />
+          <Input type="number" placeholder="Stock" {...form.register('row.stock')} />
+        </div>
+        <div className="flex justify-between">
+          <Button variant={'outline'} type="submit" onClick={() => form.reset({ row: defaultValuesUpdateRow })}>
+            Eliminar
+          </Button>
+          <Button type="submit">Actualizar</Button>
+        </div>
       </form>
     </Form>
   );
