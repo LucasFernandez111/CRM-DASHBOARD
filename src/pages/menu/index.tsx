ce63bc03fa1d90e0b675b3856fe0d5b45a29ad76
Lucas <flucasyoel@gmail.com>
2024-11-12 18:04:27 -0300

    refactor: :recycle: simplify MenuPage by using directly fetched products and adding refresh functionality

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,33 +1,14 @@
 import { Card } from '@/components/ui/card';
 import { columns, DataTable } from './components/data-table/DataTable';
-import { useNotification, useProducts } from '@/hooks';
-import { useEffect, useState } from 'react';
-import { SheetData } from './components/data-table/DataTable/columns';
-import { structureProductsData } from './utils';
+import { useProducts } from '@/hooks';
 
 export const MenuPage = () => {
-  const { products } = useProducts();
-  const { alertError, alertInfo } = useNotification();
-  const [productsData, setProductsData] = useState<SheetData[]>([
-    {
-      category: '-',
-      subcategory: '-',
-      price: '0',
-      stock: 0,
-    },
-  ]);
-
-  useEffect(() => {
-    if (!products) alertError('Error al cargar los productos');
-    if (products.length < 0) alertInfo('No hay productos');
-
-    setProductsData(structureProductsData(products));
-  }, [products]);
+  const { products, handleRefresh } = useProducts();
 
   return (
     <main className=" col-span-11   h-full max-h-screen overflow-auto bg-customSteelblue p-4">
       <Card>
-        <DataTable columns={columns} data={productsData} />
+        <DataTable columns={columns} data={products} onRefreshFetchTable={handleRefresh} />
       </Card>
     </main>
   );
