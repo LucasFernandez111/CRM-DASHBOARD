a3e4c484fe3107976f5cd09a2f3ec964be040d04
Lucas <flucasyoel@gmail.com>
2024-12-20 14:28:44 -0300

    update

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -16,6 +16,7 @@ export const AppRouter: React.FC<{}> = () => {
     <Routes>
       <Route path="/" element={<Navigate to={PrivateRoutes.SALES} />} />
 
+      <Route path={PublicRoutes.LOGIN} element={<LoginPage />} />
       <Route path={PublicRoutes.AUTH_GOOGLE} element={<AuthCallbackGuard />} />
       <Route element={<AuthGuard />}>
         <Route path="/" element={<RouterLayout />}>
